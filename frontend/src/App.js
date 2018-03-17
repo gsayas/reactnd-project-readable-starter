a089484f14ade6cfea431710b540dd424592a0ab
Giovanny Sayas <giovanny.sayas@akendos.com>
2018-03-17 18:35:17 +0000

    feat: setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  test = () => {
+    return 0;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Readable.
+      </div>
+    );
+  }
+}
+
+export default App;
