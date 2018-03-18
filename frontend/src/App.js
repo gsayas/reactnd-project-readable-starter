b963d1559ec7f723ff197e88f4659761be34124b
Giovanny Sayas <giovanny.sayas@akendos.com>
2018-03-18 21:05:51 +0000

    feat: build PostsAPI core

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as PostsAPI from './utils/PostsAPI';
 
 class App extends Component {
 
-  test = () => {
-    return 0;
+  state = {
+    posts: [],
+  }
+
+  //Load all the books in my 'library' (server) and put them in my state
+  componentDidMount(){
+    PostsAPI.getAllPosts().then((posts) => {
+      this.setState({posts: posts})
+    });
   }
 
   render() {
+    const {posts} = this.state;
+
     return (
       <div className="App">
-        Readable.
+        {posts.map((post) => (
+          <div key={post.id}>
+            {post.title}
+          </div>
+        ))}
       </div>
     );
   }
---
+++ frontend/src/utils/PostsAPI.js
@@ -0,0 +1,43 @@
+const api = "http://localhost:3001";
+
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token;
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8);
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getPost = (postId) =>
+  fetch(`${api}/posts/${postId}`, { headers })
+    .then(res => res.json())
+    .then(data => data.post)
+
+export const getAllPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+export const update = (book, shelf) =>
+  fetch(`${api}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json())
+
+export const search = (query) =>
+  fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  }).then(res => res.json())
+    .then(data => data.books)
